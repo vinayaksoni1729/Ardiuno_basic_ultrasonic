2e72a89635d9488da01891dcb14bc7ba151c2737
vinayaksoni1729 <113187290+vinayaksoni1729@users.noreply.github.com>
2023-04-08 01:09:39 +0530

    Add files via upload

---
+++ radar_with_led.ino
@@ -0,0 +1,28 @@
+#define trig 7
+#define echo 6
+#define ledpin 8
+int time;
+float dist;
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(trig,OUTPUT);
+  pinMode(echo,INPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your  main code here, to run repeatedly:
+digitalWrite(trig,LOW);
+delayMicroseconds(2);
+digitalWrite(trig,HIGH);
+delayMicroseconds(10);
+time=pulseIn(echo,HIGH);
+dist=0.34*time/2;
+dist=dist/10;
+Serial.println(dist);
+if(dist<10){
+  digitalWrite(ledpin,HIGH);}
+else{
+  digitalWrite(ledpin,LOW);}
+delay(500);
+}
